f80300dfc08962933127ecc1fdbcd40bfa349a10
Divija2004 <divijasuresh19@gmail.com>
2026-09-24 18:35:38 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class question2 {
+
+    public static int minJumps(int[] nums) {
+
+        if (nums.length <= 1) {
+            return 0;
+        }
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= nums.length - 1) {
+                    break;
+                }
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = minJumps(nums);
+
+        System.out.println("Minimum number of jumps: " + result);
+
+        sc.close();
+    }
+}
